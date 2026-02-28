78471bc33868651b24bea90e5525cd392f41b77d
CodyCBakerPhD <codycbakerphd@gmail.com>
2026-02-28 12:06:15 -0500

    add minification

--- code/src/my_work_history/__init__.py
+++ code/src/my_work_history/__init__.py
@@ -1,8 +1,10 @@
 from ._fetch_info import fetch_info_for_date
 from ._dump import dump_specific_info, dump_info_for_date
+from ._update import update
 
 __all__ = [
     "dump_specific_info",
     "dump_info_for_date",
     "fetch_info_for_date",
+    "update",
 ]
