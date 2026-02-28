1a2e1c3e3bb9d6c16ce01e2b189d3618e4a3743f
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-02-28 14:25:00 -0500

    Debugging (#3)
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * update
    
    * debug
    
    ---------
    
    Co-authored-by: CodyCBakerPhD <codycbakerphd@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- code/src/my_work_history/_cli.py
+++ code/src/my_work_history/_cli.py
@@ -21,21 +21,21 @@ def _mywork_cli():
     "--recency",
     "past_number_of_days",
     type=int,
-    default=None,
+    required=True,
     help="Number of most recent days to fetch. Smart updating still applies.",
 )
 @rich_click.option(
     "--request",
     "request_type",
     type=rich_click.Choice(["rest", "graphql"], case_sensitive=False),
-    default="rest",
+    required=True,
     help="The type of API request to use when fetching information (e.g., 'rest' or 'graphql').",
 )
 def _mywork_update_cli(
     directory: str,
     username: str,
-    past_number_of_days: int | None = None,
-    request_type: typing.Literal["rest", "graphql"] = "rest",
+    past_number_of_days: int,
+    request_type: typing.Literal["rest", "graphql"],
 ) -> None:
     directory = pathlib.Path(directory)
 
